1fcf2a551253f27f68e570368209d77caf6207cd
Pavel <sfcc@pilena.co>
2020-06-11 18:34:47 +0300

    fix newsletter subscription trigger events

--- cartridges/bm_emarsys/cartridge/controllers/NewsletterSubscription.js
+++ cartridges/bm_emarsys/cartridge/controllers/NewsletterSubscription.js
@@ -7,7 +7,7 @@ var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 var BMEmarsysHelper = require('*/cartridge/scripts/helpers/BMEmarsysHelper');
 
 /**
- * @description this function is colled with bm_extensions
+ * @description this function is called with bm_extensions
  */
 server.get('ShowNewsletterSubscription', server.middleware.https, function (req, res, next) {
     var tabsAttr = BMEmarsysHelper.getTabsAttr('EmarsysNewsletterSubscription', 'EmarsysSubscriptionType');
@@ -31,14 +31,25 @@ server.post('SaveNewsletter', server.middleware.https, function (req, res, next)
     var newsletteForm = server.forms.getForm('newsletterSub');
     var newsletteFormObj = newsletteForm.toObject();
 
+    var events = BMEmarsysHelper.getExternalEvents('EmarsysExternalEvents', 'StoredEvents', 'newsletterSubscriptionResult');
+    events.forEach(function (event) {
+        if (this.externalEventOptin === event.id) {
+            this.externalEventOptinName = event.name;
+        }
+
+        if (this.externalEventOptinAfterConfirmation === event.id) {
+            this.externalEventOptinAfterConfirmationName = event.name;
+        }
+    }, newsletteFormObj);
+
     if (newsletteFormObj.subscriptionType) {
         try {
             Transaction.wrap(function () {
                 var currentCustomObject = CustomObjectMgr.getCustomObject('EmarsysNewsletterSubscription', newsletteFormObj.subscriptionType);
 
-                currentCustomObject.custom.optInExternalEvent = newsletteFormObj.externalEventOptin;
+                currentCustomObject.custom.optInExternalEvent = newsletteFormObj.externalEventOptinName;
                 currentCustomObject.custom.optInStrategy = newsletteFormObj.subscriptionStrategy;
-                currentCustomObject.custom.optInExternalEventAfterConfirmation = newsletteFormObj.externalEventOptinAfterConfirmation;
+                currentCustomObject.custom.optInExternalEventAfterConfirmation = newsletteFormObj.externalEventOptinAfterConfirmationName;
             });
             res.json({
                 success: true
