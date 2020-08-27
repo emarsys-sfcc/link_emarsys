9c56a7430617a344484333238806766389e660b1
Pavel <sfcc@pilena.co>
2020-08-27 13:20:31 +0300

    update documentation

--- cartridges/int_emarsys_sfra/cartridge/scripts/helpers/emarsysSFRAHelper.js
+++ cartridges/int_emarsys_sfra/cartridge/scripts/helpers/emarsysSFRAHelper.js
@@ -430,7 +430,7 @@ function checkoutSubscription(res, billingData) {
     if (Site.getCurrent().getCustomPreferenceValue('emarsysEnabled')) {
         args.SubscriptionType = 'checkout';
         args.Email = billingData.email && billingData.email.value;
-        args.SubscribeToEmails = session.forms.billing.subscribe.value;
+        args.SubscribeToEmails = session.forms.billing.addToEmailList.checked;
         args = newsletterHelper.getCustomerData(args); // get customer data
         // process the request and form data
         processor(args, res);
